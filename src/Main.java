a0ae8e31d9459296f5dce0546d2d116868d243c2
Dmitriy Mekheda <mekhedadima@gmail.com>
2024-09-21 07:35:21 +0500

    reformated code

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,28 @@
 import java.util.Scanner;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        // Объявляете переменные для входных данных и
-        // параметров программы: начального счёта,
-        // суммы пополнения и тп
-        int st; // первоначальный баланс
-        int add; // сумма пополнения
-        int bon; // количество бонусных рублей
-        int kon; // итоговый баланс
+public class Main {
+    public static void main(String[] args) {
+        int balance; // первоначальный баланс
+        int added_sum; // сумма пополнения
+        int bonus; // количество бонусных рублей
+        int final_sum; // итоговый баланс
 
         Scanner in = new Scanner(System.in);
         System.out.print("Изначальный баланс: ");
-        st = in.nextInt();
+        balance = in.nextInt();
 
         System.out.print("Сумма пополнения: ");
-        add = in.nextInt();
+        added_sum = in.nextInt();
 
-        if (add >= 1000)
-            bon = add / 100;
-        else
-            bon = 0;
+        if (added_sum >= 1000) {
+            bonus = added_sum / 100;
+        } else {
+            bonus = 0;
+        }
 
-        kon = st + add + bon;
-        System.out.print("Итоговый баланс: " + kon);
-        // Также можно обойтись и без переменной "kon", залив сумму в скобочки в последней кодовой строке,
-        // чтобы это выглядело так: System.out.print("Итоговый баланс: " + (st + add + bon));
+        final_sum = balance + added_sum + bonus;
+        System.out.print("Итоговый баланс: " + final_sum);
+        // Также можно обойтись и без переменной "final_sum", залив сумму в скобочки в последней кодовой строке,
+        // чтобы это выглядело так: System.out.print("Итоговый баланс: " + (balance + added_sum + bonus));
     }
 }
