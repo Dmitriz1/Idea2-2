d65591767da67033223e53032b028a770a13d2c3
Дмитрий Мехеда <114097534+Dmitriz1@users.noreply.github.com>
2024-09-22 07:31:49 +0500

    Names now in camelstyle

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int balance; // первоначальный баланс
-        int added_sum; // сумма пополнения
+        int AddedSum; // сумма пополнения
         int bonus; // количество бонусных рублей
-        int final_sum; // итоговый баланс
+        int FinalSum; // итоговый баланс
 
         Scanner in = new Scanner(System.in);
         System.out.print("Изначальный баланс: ");
         balance = in.nextInt();
 
         System.out.print("Сумма пополнения: ");
-        added_sum = in.nextInt();
+        AddedSum = in.nextInt();
 
-        if (added_sum >= 1000) {
-            bonus = added_sum / 100;
+        if (AddedSum >= 1000) {
+            bonus = AddedSum / 100;
         } 
         else {
             bonus = 0;
         }
 
-        final_sum = balance + added_sum + bonus;
-        System.out.print("Итоговый баланс: " + final_sum);
-        // Также можно обойтись и без переменной "final_sum", залив сумму в скобочки в последней кодовой строке,
-        // чтобы это выглядело так: System.out.print("Итоговый баланс: " + (balance + added_sum + bonus));
+        FinalSum = balance + AddedSum + bonus;
+        System.out.print("Итоговый баланс: " + FinalSum);
+        // Также можно обойтись и без переменной "FinalSum", залив сумму в скобочки в последней кодовой строке,
+        // чтобы это выглядело так: System.out.print("Итоговый баланс: " + (balance + AddedSum + bonus));
     }
 }
