9846a8f7b9d870134365415af6057f655f833f5b
Дмитрий Мехеда <114097534+Dmitriz1@users.noreply.github.com>
2024-09-24 07:42:27 +0500

    Made it easier

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,17 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        int balance; // первоначальный баланс
-        int addedSum; // сумма пополнения
+        int balance = 100; // первоначальный баланс
+        int addedSum = 1100; // сумма пополнения
         int bonus; // количество бонусных рублей
         int finalSum; // итоговый баланс
 
-        Scanner in = new Scanner(System.in);
-        System.out.print("Изначальный баланс: ");
-        balance = in.nextInt();
-
-        System.out.print("Сумма пополнения: ");
-        addedSum = in.nextInt();
-
         if (addedSum >= 1000) {
             bonus = addedSum / 100;
-        } 
-        else {
+        } else {
             bonus = 0;
         }
 
         finalSum = balance + addedSum + bonus;
         System.out.print("Итоговый баланс: " + finalSum);
-        // Также можно обойтись и без переменной "finalSum", залив сумму в скобочки в последней кодовой строке,
-        // чтобы это выглядело так: System.out.print("Итоговый баланс: " + (balance + addedSum + bonus));
     }
 }
